7d980c68ec73c2620d39bc75f2d2234f7c9e0352
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-01-06 00:33:04 -0500

    Started Exercise 1, Added assets

---
+++ Exercises/exercise1/js/script.js
@@ -0,0 +1,29 @@
+/**
+Gem Hunt - Exercise 1
+Alex Cho
+
+Pretty much the same type of mechanics from the activity. Just enhanced and updated.
+*/
+
+"use strict";
+
+function preload() {
+
+}
+
+
+function setup() {
+  //To be proper, I added this
+  userStartAudio();
+
+  //creating the canvas
+  createCanvas(1280, 720);
+
+
+
+}
+
+
+function draw() {
+  
+}
