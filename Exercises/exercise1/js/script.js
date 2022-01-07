86182d9ec0d31f3ce6da41c2b4b08dbeb96277eb
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-01-06 22:26:46 -0500

    E1 Setting up some of the basic parameters and variables.

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -7,16 +7,44 @@ Pretty much the same type of mechanics from the activity. Just enhanced and upda
 
 "use strict";
 
+// Constants for images
+const NUM_GEM_IMAGES = 10;
+const GEM_IMAGE_PREFIX = `assets/images/Gems/gem/`;
+const DIAMOND_IMAGE = `assets/images/Gems/diamond.png`;
+
+// Number of images to display
+const NUM_EASY = 50;
+
+//Arrays for gem images
+let gemImages = [];
+
+//Arrays for gem objects
+let gems = [];
+
+//defining the image of the diamond
+let diamondImage;
+
+//defining the diamond
+let diamond;
+
+//Background Color
+let bgRed = 173;
+let bgGreen = 133;
+let bgBlue = 130;
+
+//Set the starting state
+let state = 'title';
+
 function preload() {
 
 }
 
 
 function setup() {
-  //To be proper, I added this
+  // To be proper, I added this
   userStartAudio();
 
-  //creating the canvas
+  // creating the canvas
   createCanvas(1280, 720);
 
 
@@ -25,5 +53,7 @@ function setup() {
 
 
 function draw() {
-  
+  //Setting the background color
+  background(bgRed, bgGreen, bgBlue);
+
 }
