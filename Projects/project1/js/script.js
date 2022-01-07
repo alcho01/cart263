8190aff76a3eeab350700021ca253f3fdc28dec2
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-01-07 17:20:58 -0500

    P1 Preparing Project 1

---
+++ Projects/project1/js/script.js
@@ -0,0 +1,15 @@
+/**
+Project 1 - Get Out
+Alex Cho
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+function preload() {}
+
+function setup() {}
+
+function draw() {}
