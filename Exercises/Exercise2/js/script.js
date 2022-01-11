8f4ea2a6d86b18ae2cf9e509ba6ffec0d56a0d7c
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-01-11 17:32:14 -0500

    E2 Creating arrays for q and a. still might change idea depending on difficulty.

--- Exercises/Exercise2/js/script.js
+++ Exercises/Exercise2/js/script.js
@@ -1,12 +1,69 @@
 /**
-Title of Project
-Author Name
+Exercise 2
+Alex Cho
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Questions and answers from https://jeopardyquestions.com/
 */
 
 "use strict";
+
+
+//Make an array for the questions and answers
+const category1Questions = [
+  //IN THE KITCHEN QUESTIONS
+  "A pestle is used to grind spices & other foods; this pestle's partner is the bowl in which the food is ground",
+  "Despite its name it's a slicing tool not a guitar-like instrument so don't strum it",
+  "This brilliant yellow ultra-spendy spice is sold powdered or as threads (the flower's stigma)",
+];
+
+const category1Answers = [
+  //IN THE KITCHEN ANSWERS
+  "mortar",
+  "mandoline",
+  "saffron",
+];
+
+const category2Questions = [
+  //B + 2 LETTERS QUESTIONS
+  "To clear plates",
+  "Cubic kite",
+  "Stated goal of taking tricks",
+];
+
+const category2Answers = [
+  //B + 2 LETTERS ANSWERS
+  "bus",
+  "box",
+  "bid",
+];
+
+const category3Questions = [
+  //TRIPLE ALLITERATION QUESTIONS
+  "This pencil-&-paper game is also known as Xs & Os",
+  "Only around since the early 1990s it takes you practically anywhere you want to go--on the Internet",
+  "Relationship of Queen Elizabeth to Queen Victoria",
+];
+
+const category3Answers = [
+  //TRIPLE ALLITERATION ANSWERS
+  "tic tac toe",
+  "world wide web",
+  "great great granddaughter",
+];
+
+const category4Questions = [
+  //A 9-LETTER NOUN QUESTIONS
+  "This fruit may sometimes appear on a peach tree",
+  "It's a feeling of sentimental yearning for the past",
+  "Name shared by the first operational stealth aircraft & a nocturnal bird their prey never sees either one coming",
+];
+
+const category4Answers = [
+  //A 9-LETTER NOUN ANSWERS
+  "nectarine",
+  "nostalgia",
+  "nighthawk",
+];
 
 
 
