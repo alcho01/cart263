e7242a1e5caf904fa29c373a578fe1a47679ef97
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-01-13 22:28:05 -0500

    E2 Added responsive voice to narrate the question

--- Exercises/Exercise2/js/BoardScreen.js
+++ Exercises/Exercise2/js/BoardScreen.js
@@ -167,18 +167,21 @@ class BoardScreen {
         state = 'Column1';
         //Display the specified question
         this.question = category1Questions[currentCategory1Questions];
+        responsiveVoice.speak("A pestle is used to grind spices & other foods; this pestle's partner is the bowl in which the food is ground");
       }
     }
     if (mouseX > this.box2.x && mouseX < this.box2.x2) {
       if (mouseY > this.box2.y && mouseY < this.box2.y2) {
         state = 'Column1';
         this.question = category1Questions[currentCategory1Questions + 1];
+        responsiveVoice.speak("Despite its name it's a slicing tool not a guitar-like instrument so don't strum it");
       }
     }
     if (mouseX > this.box3.x && mouseX < this.box3.x2) {
       if (mouseY > this.box3.y && mouseY < this.box3.y2) {
         state = 'Column1';
         this.question = category1Questions[currentCategory1Questions + 2];
+        responsiveVoice.speak("This brilliant yellow ultra-spendy spice is sold powdered or as threads, the flower's stigma");
       }
     }
 
@@ -187,18 +190,21 @@ class BoardScreen {
       if (mouseY > this.box4.y && mouseY < this.box4.y2) {
         state = 'Column2';
         this.question2 = category2Questions[currentCategory2Questions];
+        responsiveVoice.speak("To clear plates");
       }
     }
     if (mouseX > this.box5.x && mouseX < this.box5.x2) {
       if (mouseY > this.box5.y && mouseY < this.box5.y2) {
         state = 'Column2';
         this.question2 = category2Questions[currentCategory2Questions + 1];
+        responsiveVoice.speak("Cubic kite");
       }
     }
     if (mouseX > this.box6.x && mouseX < this.box6.x2) {
       if (mouseY > this.box6.y && mouseY < this.box6.y2) {
         state = 'Column2';
         this.question2 = category2Questions[currentCategory2Questions + 2];
+        responsiveVoice.speak("Stated goal of taking tricks");
       }
     }
 
@@ -207,18 +213,21 @@ class BoardScreen {
       if (mouseY > this.box7.y && mouseY < this.box7.y2) {
         state = 'Column3';
         this.question3 = category3Questions[currentCategory3Questions];
+        responsiveVoice.speak("This pencil-&-paper game is also known as exes & o's");
       }
     }
     if (mouseX > this.box8.x && mouseX < this.box8.x2) {
       if (mouseY > this.box8.y && mouseY < this.box8.y2) {
         state = 'Column3';
         this.question3 = category3Questions[currentCategory3Questions + 1];
+        responsiveVoice.speak("Only around since the early 1990s it takes you practically anywhere you want to go--on the Internet");
       }
     }
     if (mouseX > this.box9.x && mouseX < this.box9.x2) {
       if (mouseY > this.box9.y && mouseY < this.box9.y2) {
         state = 'Column3';
         this.question3 = category3Questions[currentCategory3Questions + 2];
+        responsiveVoice.speak("Relationship of Queen Elizabeth to Queen Victoria");
       }
     }
 
@@ -227,18 +236,21 @@ class BoardScreen {
       if (mouseY > this.box10.y && mouseY < this.box10.y2) {
         state = 'Column4';
         this.question4 = category4Questions[currentCategory4Questions];
+        responsiveVoice.speak("This fruit may sometimes appear on a peach tree");
       }
     }
     if (mouseX > this.box11.x && mouseX < this.box11.x2) {
       if (mouseY > this.box11.y && mouseY < this.box11.y2) {
         state = 'Column4';
         this.question4 = category4Questions[currentCategory4Questions + 1];
+        responsiveVoice.speak("It's a feeling of sentimental yearning for the past");
       }
     }
     if (mouseX > this.box12.x && mouseX < this.box12.x2) {
       if (mouseY > this.box12.y && mouseY < this.box12.y2) {
         state = 'Column4';
         this.question4 = category4Questions[currentCategory4Questions + 2];
+        responsiveVoice.speak("Name shared by the first operational stealth aircraft & a nocturnal bird --their prey never sees either one coming");
       }
     }
   }
