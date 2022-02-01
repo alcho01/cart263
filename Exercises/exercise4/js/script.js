dd653bf88f2333b3adb856aad02bdc562131a802
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-02-01 15:18:21 -0500

    E4 Display the bubble and the pin.

--- Exercises/exercise4/js/script.js
+++ Exercises/exercise4/js/script.js
@@ -17,6 +17,28 @@ let handpose;
 //Predictions made from handpose
 let predictions = [];
 
+//The bubble to pop
+let bubble;
+//The pin
+let pin = {
+  tip: {
+    x: undefined,
+    y: undefined,
+    stroke: 145,
+    strokeWeight: 2,
+  },
+  head: {
+    x: undefined,
+    y: undefined,
+    size: 20,
+  },
+  fill: {
+    r: 0,
+    g: 16,
+    b: 189,
+  }
+};
+
 function preload() {
 
 }
@@ -36,13 +58,55 @@ function setup() {
 
   });
 
-  //Listen for predictions from handpose 
+  //Listen for predictions from handpose
   handpose.on(`predict`, function (results) {
     predictions = results;
   });
 
+  //Create the bubble
+  bubble = {
+    settings: {
+      x: 80,
+      y: 80,
+      size: 100,
+      vx: 0,
+      vy: -2,
+    },
+    fill: {
+      r: 124,
+      g: 232,
+      b: 242,
+      a: 100,
+    }
+  };
 }
 
 function draw() {
 
+}
+
+//Display the bubble
+function displayBubble() {
+  push();
+  noStroke();
+  fill(bubble.fill.r, bubble.fill.g, bubble.fill.b, bubble.fill.a);
+  ellipse(bubble.settings.x, bubble.settings.y, bubble.settings.size);
+  pop();
+}
+
+//Display the pin and head
+function displayPin() {
+  //The pin
+  push();
+  stroke(pin.tip.stroke);
+  strokeWeight(pin.tip.strokeWeight);
+  line(pin.tip.x, pin.tip.y, pin.head.x, pin.head.y);
+  pop();
+
+  //The Head
+  push();
+  fill(pin.fill.r, pin.fill.g, pin.fill.b);
+  noStroke();
+  ellipse(pin.head.x, pin.head.y, pin.head.size);
+  pop();
 }
