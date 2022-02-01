26a940abbb5fcc225725d8bf334ea0719fc6de62
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-02-01 13:19:06 -0500

    E4 Setup the webcam and canvas

--- Exercises/exercise4/js/script.js
+++ Exercises/exercise4/js/script.js
@@ -6,11 +6,24 @@ Alex Cho
 
 "use strict";
 
+//Canvas Dimensions
+let canvasWidth = 640;
+let canvasHeight = 480;
+
+//Webcam
+let video;
+
 function preload() {
 
 }
 
 function setup() {
+  //Setup the canvas
+  createCanvas(canvasWidth, canvasHeight);
+
+  //Setup the video //Hide it
+  video = createCapture(VIDEO);
+  video.hide();
 
 }
 
