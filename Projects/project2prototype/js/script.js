8f57d095b92ac00ffacd91fb6ba38ff79624dcaa
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-02-22 22:13:05 -0500

    P2P Simple setup of canvas and background

--- Projects/project2prototype/js/script.js
+++ Projects/project2prototype/js/script.js
@@ -7,14 +7,25 @@ Alex Cho
 
 "use strict";
 
+//Set the canvas dimensions
+let canvas = {
+  width: 1280,
+  height: 720,
+};
+
+//set the background color to black
+let bgColor = 0;
+
 function preload() {
 
 }
 
 function setup() {
-
+  //create the canvas
+  createCanvas(canvas.width, canvas.height);
 }
 
 function draw() {
-
+  //set the background 
+  background(bgColor);
 }
